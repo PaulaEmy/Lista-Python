04c210245a0c9e9ed819fe38b35b53ccf7393ab4
Paula Emy <paulaemy8262999@gmail.com>
2025-03-14 08:06:16 -0300

    Add files via upload

---
+++ Lista 3/ex1.py
@@ -0,0 +1,3 @@
+num = int(input("Número entre 0 e 10: "))
+while num < 0 or num > 10:
+    num = int(input("Tente novamente: "))
---
+++ Lista 3/ex2.py
@@ -0,0 +1,6 @@
+user = input("Usuário: ")
+senha = input("Senha: ")
+while user == senha:
+    print("Tente novamente")
+    user = input("Usuário: ")
+    senha = input("Senha: ")
